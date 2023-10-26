ed1a7248a8f32246a6709957a843025056f9022f
RandomCad <mmont4850@gmail.com>
2023-10-26 12:49:27 +0200

    Initial network setup

---
+++ network-clin-net-2-2.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+#netplan files are worked alpabeticaly and the settings of the latest file is taken. If therfor the settings are writen in the last file thei will take effecte
+for i in $(ls /etc/netplan/ | grep '.*yaml'); do
+	echo $i
+done | sort > zwi
+
+#get the numbering of the higest numberd file
+files=$(tail -1 zwi)
+FileNum=$(echo $files | grep -o -E '[0-9]+' )
+
+#creat the file name of the new file
+if (( $FileNum < 9 )) ; then
+  FILE=/etc/netplan/0$(($FileNum+1))-Inter-VM-Comm.yaml
+else
+  FILE=/etc/netplan/$(($FileNum+1))-Inter-VM-Comm.yaml
+fi
+
+echo 'network:
+  version: 2
+  renderer: NetworkManager
+  ethernets:
+    enp0s8:
+      dhcp4: no
+      addresses: [192.168.2.3/24]
+      routes:
+      - to: default
+        via: 192.168.2.1
+      - to: 192.168.1.0/24
+        via: 192.168.2.1' >> $FILE
+
+sudo chmod 600 $FILE
+
+sudo netplan apply
+      
+
+#cleanup
+rm zwi -f
+
+
+
