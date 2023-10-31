658c538c0635bbecdcfdbc3be582923ac932e7c1
RandomCad <mmont4850@gmail.com>
2023-10-31 10:05:00 +0100

    save namserver added to net1/2 router

--- network-serv-net-1.sh
+++ network-serv-net-1.sh
@@ -46,6 +46,13 @@ fi
 
 sudo netplan apply
       
+#add nameserver
+if grep -q 'nameserver 8.8.8.8' /etc/resolv.conf || grep -q 'nameserver 8.8.4.4' /etc/resolv.conf ; then
+  #nameserver already set
+  echo 'nameserver is set corectly'
+else
+  echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+fi
 
 #cleanup
 rm zwi -f
