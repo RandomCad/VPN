f24c6a6bed7c90872fff180dbd0e8cd10420173e
RandomCad <mmont4850@gmail.com>
2023-11-26 09:41:31 +0100

    Fix for now ip afther reconfig on enp0s3

--- Router-N-M.sh
+++ Router-N-M.sh
@@ -45,7 +45,7 @@ echo "$Blue Setting up NAT-networking. Start $Work of $Total $White"
 echo "$Blue Running corntab setting. Start $Work.1 of $Total $White"
 #making shure the nat networking is run on reboot
 if sudo crontab -l -u root | grep -q '@reboot sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE' ; then
-  echo '...'
+  echo "$Green Crontabs already set $White"
 else
   { sudo crontab -l -u root ; echo '@reboot sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE' ; } | sudo crontab -u root -
 fi
@@ -59,6 +59,8 @@ echo "$Blue Aktivating NAT-networking for now. Done $Work.2 of $Total $White"
 echo "$Blue Setting up NAT-networking. Done $Work of $Total $White"
 Work=$(($Work + 1))
 
+#Avther the reset the networkinterface dosen't jet have a ip address. It gets an Ip throw this ping call.
+ping -qI enp0s3 -c 1 8.8.8.8 > /dev/null  
 
 echo "$Blue Checking network. Start $Work of $Total $White"
 Error=0
